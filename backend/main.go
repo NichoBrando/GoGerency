02cf53b7e46f2f09d9ccb81aba6fd199e56c7487
Nicholas <nicholas.computacao@outlook.com>
2020-09-01 13:08:44 -0300

    API created

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+  "log"
+  "github.com/gorilla/mux"
   "fmt"
+  "net/http"
+  "encoding/json"
 )
 
+/*json.NewEncoder(w).Encode(variable) is very important
+with this, we can return data to frontend*/
+func home(w http.ResponseWriter, r *http.Request){
+  json.NewEncoder(w).Encode()
+}
+
+//Starting API with mux lib, to get HTTP methods.
 func main(){
-  fmt.Println("Meu hello world!")
+  fmt.Println("Rodando servidor na porta 8080")
+  router := mux.NewRouter()
+  router.HandleFunc("/", home).Methods("GET")
+  log.Fatal(http.ListenAndServe(":8080", router))
 }
